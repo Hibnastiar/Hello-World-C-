0b7645269b91ab5a292a8e8c235453fc0e8d9e8d
Nur Hibnasitar <87582881+Hibnastiar@users.noreply.github.com>
2021-09-25 12:39:43 +0700

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Mencoba Membuat Hello World dengan C++, 
+	
+	
+	cout <<"Hello World"<< endl ; // (<<endl;) Enter
+	cout <<"Hello World \n" ; // (\n) Enter
+	cout <<"Hello World \t" ; // (\t) Tap 
+	cout <<"Hello World";
+	
+	return 0;
+}
